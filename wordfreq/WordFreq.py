77fb13fc5d61a10972db66fc51c2c4a5284dafa6
kiku <kiku@jp.ibm.com>
2022-12-29 15:08:50 +0900

    use log() and jsonpickle
     On branch master
     Changes to be committed:
    	modified:   Dockerfile
    	modified:   WordFreq.py

--- wordfreq/WordFreq.py
+++ wordfreq/WordFreq.py
@@ -6,9 +6,11 @@ import json
 import os
 import datetime
 import urllib
+import asyncio
 
 import queue
 import threading
+import jsonpickle
 
 app = Flask(__name__)
 
@@ -16,6 +18,9 @@ PORT = os.environ.get('PORT', 8000)
 TOKENIZER_URL = os.environ.get('TOKENIZER_URL', 'http://127.0.0.1:8001')
 MAPPER_URL = os.environ.get('MAPPER_URL', 'http://127.0.0.1:8002')
 REDUCER_URL = os.environ.get('REDUCER_URL', 'http://127.0.0.1:8003')
+
+def log(message):
+    print(str(datetime.datetime.now()) + ": " + message)
 
 @app.route('/CountWords', methods=['POST'])
 def count_words():
@@ -28,28 +33,29 @@ def count_words():
     text_data = request.get_data(as_text=True)
 #   print(text_data)
 
-    print(str(datetime.datetime.now()) + ": Send all request to handler")
+    log("Send all request to handler")
     for line in split_lines(text_data):
         if (len(line) > 0):
-            t = threading.Thread(target = handle_line, args=(word_space, line))
-            t.start()
-            threads.append(t)
+            handle_line(word_space, line);
+#           t = threading.Thread(target = handle_line, args=(word_space, line))
+#           t.start()
+#           threads.append(t)
 
-    print(str(datetime.datetime.now()) + ": Waiting all thread termination")
-    [t.join() for t in threads]
+    log("Waiting all thread termination")
+#   [t.join() for t in threads]
 
     word_list = []
     while not word_space.empty():
         word_list.extend(word_space.get())
 
-    print(str(datetime.datetime.now()) + ": Counting Words")
+    log("Counting Words")
     count = count_words(word_list)
 
     result  = sorted(count.items(), reverse=True, key = lambda word : word[1])[0:25]
-    print(str(datetime.datetime.now()) + ": Sorted and return the result")
+    log("Sorted and return the result")
 
     print(f"RESULT: {result}")
-    return result
+    return jsonpickle.encode(result)
 
 def split_lines(data):
     return data.split('\n')
@@ -60,18 +66,12 @@ def count_words(list):
     
 
 def handle_line(queue, line):
-#   print(line)
 
     response = requests.get(TOKENIZER_URL + '?line=' + urllib.parse.quote(line))
     word_list = response.json()
 
-#   for word in word_list:
-#       print(f"DEBUG: {word}")
-
     response = requests.post(MAPPER_URL, data = json.dumps(word_list))
     word_dicts = response.json()
-
-#   print(f"DEBUG: {word_dicts}")
 
     queue.put(word_dicts)
 
